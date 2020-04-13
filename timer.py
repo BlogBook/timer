2e3195bb639c8e578646746c4e2f7726f33b64cf
dellstop123 <guneetsinghbali@gmail.com>
2020-04-13 12:31:28 +0530

    updated

--- timer.py
+++ timer.py
@@ -1,10 +1,34 @@
 import threading
+import time
+from atpbar import flush, atpbar
+import random
+
+# def gfg():
+#     count = 1
+#     print("Hub\n", count+1)
 
 
-def gfg():
-    count = 1
-    print("Hub\n", count+1)
+# timer = threading.Timer(5.0, gfg)
+# print(timer.start())
+# timer.start()
+
+def run_with_threading():
+    nthreads = 1
+
+    def task(n, name):
+        for i in atpbar(range(n), name=name):
+            time.sleep(0.0001)
+    threads = []
+    for i in range(nthreads):
+        name = 'thread {}'.format(i)
+        n = random.randint(5, 100)
+        t = threading.Thread(target=task, args=(n, name))
+        t.start()
+        threads.append(t)
+    for t in threads:
+        t.join()
+
+    flush()
 
 
-timer = threading.Timer(5.0, gfg)
-timer.start()
+run_with_threading()
